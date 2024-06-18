cd6e45e5a23ccad68cfe4a22f1cce9f511a95482
mwadt <mbrincko93@gmail.com>
2024-06-18 11:31:59 -0700

    set up home, navbar, and create components

---
+++ final-project-front-end/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className='body'>
+        <h1>Welcome to the Travel App</h1>
+        <p>Placeholder text blurb</p>
+        <button>Start Planning</button>
+    </div>
+  )
+}
+
+export default Home;
---
+++ final-project-front-end/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <a href="/">New Itenerary</a>
+        <a href="index">All Iteneraries</a>
+    </div>
+  );
+}
+
+export default Navbar;
